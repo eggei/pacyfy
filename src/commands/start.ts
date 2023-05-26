5d09fd3e8b482e84d7499460222d77258e511143
Ege Ercan <89795043+meercan@users.noreply.github.com>
2023-05-25 22:00:17 -0400

    Make service run command execution synchronous

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,8 +1,8 @@
-import { Args, Command, Flags } from "@oclif/core";
+import { Args, Command, Flags, ux } from "@oclif/core";
 import { readFileSync } from "fs";
 import { join } from "path";
 import getConfig from "../getConfig";
-import { spawn } from "child_process";
+import { spawn, spawnSync } from "child_process";
 
 export default class Start extends Command {
   static description = `Starts the Cypress tests based on the given parameters`;
@@ -52,26 +52,17 @@ export default class Start extends Command {
     const { services } = config;
     for (const service of services) {
       const { name, run } = service;
-      this.log(`Running service: ${name}`);
-      const childProcess = spawn(run, { shell: true, stdio: "inherit" });
-      childProcess.on("data", (data) => {
-        this.log(data.toString());
-      });
-      childProcess.on("data", (data) => {
-        this.log(data.toString());
-      });
-      childProcess.on("error", (error) => {
-        this.error(error);
-      });
-      childProcess.on("close", (code) => {
-        if (code === 0) {
-          this.log("Done!");
-        } else {
-          this.error(`ERROR`, {
-            exit: code || undefined,
-          });
-        }
-      });
+      ux.action.start(`Running service: ${name}`);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      const result = spawnSync(run, { shell: true, stdio: "inherit" });
+      if (result.error !== undefined) {
+        this.error(result.error, { exit: 1 });
+      }
+
+      if (result.status !== 0) {
+        this.error("ERROR", { exit: result.status || undefined });
+      }
+      ux.action.stop();
     }
   }
 }
